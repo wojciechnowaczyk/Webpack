058cfce441653a67b8b89ac708f4061fb08ceda1
Wojtek Nowaczyk <wojciech.nowaczyk@espo.eu>
2022-03-08 15:20:29 +0100

    Css extracting part two

--- Tutorial/src/index.js
+++ Tutorial/src/index.js
@@ -1,4 +1,8 @@
 import HelloWordlButton from "./components/hello-world-button/hello-world-button.js";
+import Heading from "./components/heading/heading.js";
 
 const helloWordlButton = new HelloWordlButton();
 helloWordlButton.render();
+
+const heading = new Heading();
+heading.render();
