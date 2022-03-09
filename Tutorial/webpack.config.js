60bfa874c6a85504c9e5b7dab0fa66d2b338baf7
Wojtek Nowaczyk <wojciech.nowaczyk@espo.eu>
2022-03-09 10:37:37 +0100

    HTML webpack plugin

--- Tutorial/webpack.config.js
+++ Tutorial/webpack.config.js
@@ -2,13 +2,14 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
-    publicPath: "dist/",
+    publicPath: "",
   },
   mode: "none",
   module: {
@@ -55,6 +56,12 @@ module.exports = {
         "**/*", //cleans dist folder
         path.join(process.cwd(), "build/**/*"), //cleans other folders than dist
       ],
+    }),
+    new HtmlWebpackPlugin({
+      title: "Hello world",
+      meta: {
+        description: "Description",
+      },
     }),
   ],
 };
