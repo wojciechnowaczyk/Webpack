73ed51417d66df1dd6802f20f27837d0b044f99e
Wojtek Nowaczyk <wojciech.nowaczyk@espo.eu>
2022-03-14 12:41:12 +0100

    Adjusting the code for two seperated modules

---
+++ ModuleFederation/kiwi/webpack.dev.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/kiwi.js",
+
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "",
+  },
+  mode: "development",
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"),
+    index: "index.html",
+    port: 9002,
+    static: {
+      directory: path.resolve(__dirname, "./dist"),
+    },
+    devMiddleware: {
+      index: "kiwi.html",
+      wirteToDisk: true,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+          },
+        },
+      },
+      {
+        test: /\.hbs$/,
+        use: ["handlebars-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new HtmlWebpackPlugin({
+      filename: "kiwi.html",
+      title: "Kiwi",
+      description: "Kiwi",
+      template: "src/page-template.hbs",
+    }),
+  ],
+};
